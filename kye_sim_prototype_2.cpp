5be9469011b671df8e1cf91024d8e18e95bd39e9
ph4nt0m <121640331+DoomSlayer350@users.noreply.github.com>
2026-05-10 20:00:08 +0100

    Delete kye_sim_prototype_2.cpp

--- kye_sim_prototype_2.cpp
+++
@@ -1,6 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "kye_sim_prototype_2.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, kye_sim_prototype_2, "kye_sim_prototype_2" );
